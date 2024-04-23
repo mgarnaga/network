c97724cb0e9aec7c0cf644a60821e02a2feaebbd
Maria Garnaga <mona.mfr@gmail.com>
2024-04-24 00:36:21 +0300

    pagination feature, popular posts page

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -1,5 +1,12 @@
+// global scope variables to handle pagination listeners/functions
+let selected;
+let currPage;
+let total;
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    load_posts('all')
+    // after loading DOM getting popular posts for index page and attaching main listeners
+    load_posts('popular')
     document.querySelector('#all').addEventListener('click', () => load_posts('all'));
     document.querySelector('#following').addEventListener('click', () => load_posts('following'));
     document.querySelector('#post-form').addEventListener('submit', new_post);
@@ -11,9 +18,12 @@ document.addEventListener('DOMContentLoaded', function() {
 function new_post(event) {
     event.preventDefault();
     let postValue = document.querySelector('textarea').value;
+
+    // if new post is empty returning
     if (postValue === '') {
         console.log('empty post!')
         return;
+    // posting the post on server and refreshing
     } else {
         fetch('/', {
             method: 'POST',
@@ -31,89 +41,161 @@ function new_post(event) {
     };
 };
 
-function load_posts(selection) {
+// huge main function accepting posts selection and page number
+function load_posts(selection, currentPage = 1) {
+    // showing/hiding page components according to selection
     if (selection == "following") {
         document.querySelector('#user-info').style.display = 'none';
         document.querySelector('#new-post').style.display = 'none';
+        document.querySelector('#popular').style.display = 'none';
     } else if (selection == "all") {
         document.querySelector('#user-info').style.display = 'none';
         document.querySelector('#new-post').style.display = 'block';
+        document.querySelector('#popular').style.display = 'none';
+    } else if (selection == "popular") {
+        document.querySelector('#user-info').style.display = 'none';
+        document.querySelector('#new-post').style.display = 'none';
+        document.querySelector('#popular').style.display = 'block';
     } else {
         document.querySelector('#user-info').style.display = 'block';
-        document.querySelector('#new-post').style.display = 'none';
-    };
-    document.querySelector('#posts').innerHTML = "";
-    let reqUrl = `/posts/${selection}`;
-    fetch(reqUrl)
-    .then(response => response.json())
-    .then(data => {
-        let posts = data;
-        console.log(posts);
-        posts.forEach(post => {
-        const id = post.id;
-        const poster = post.poster;
-        const content = post.content;
-        const timestamp = post.timestamp;
-        let likes = post.likes;
-        const owned = post.owned;
-        const liked = post.liked;
-        let posting = document.createElement('div');
-        posting.setAttribute('class', 'post');
-        posting.setAttribute('id', id);
-        posting.innerHTML = `<div class="user">${poster}</div> <div class="post-content"> ${content} </div> 
-        <div class="timestamp">${timestamp}</div> `;
-        const likesContent = document.createElement('div');
-        likesContent.setAttribute('class', 'likes');
-        likesContent.innerHTML = `<div class="like-count">${likes}</div>`;
-        const likeButton = document.createElement('div');
-        likeButton.setAttribute('class', 'like-btn');
-        if (liked) {
-            likeButton.innerHTML = `<i class="fa-solid fa-heart" style="font-size:20px;color:#cc241d;"></i>`;
+        // if user is in his own profile page showing new post form
+        if (selection == document.querySelector('#user').textContent) {
+            document.querySelector('#new-post').style.display = 'block';
         } else {
-            likeButton.innerHTML = `<i class="fa-regular fa-heart" style="font-size:20px;"></i>`;
-        };
-        likeButton.addEventListener('click', () => {
-            like_handling(id);
-        });
-        likesContent.appendChild(likeButton);
-        posting.appendChild(likesContent);
-        if (owned) {
-            let editButton = document.createElement('div');
-            editButton.setAttribute('class', 'edit-btn');
-            editButton.textContent = 'Edit';
-            posting.appendChild(editButton);
+            document.querySelector('#new-post').style.display = 'none';
         }
-        document.querySelector('#posts').appendChild(posting);
-    })});
+        document.querySelector('#popular').style.display = 'none';
+    };
 
-    setTimeout(function() {
-        const userList = document.querySelectorAll(".user");
-        for (let i = 0; i < userList.length; i++) {
-            let nickname = userList[i].innerText;
-            userList[i].addEventListener('click', () => load_profile(nickname));
-        };
-        const editBtns = document.querySelectorAll('.edit-btn');
-        for (let i = 0; i < editBtns.length; i++) {
-            let id = editBtns[i].parentElement.id;
-            editBtns[i].addEventListener('click', () => edit_post(id));
-        }
-    }, 50);
+    // clearing posts and removing irrelevant pagination listeners
+    document.querySelector('#posts').innerHTML = "";
+    document.querySelector('#next-page').removeEventListener('click', nextPageButtonClick);
+    document.querySelector('#prev-page').removeEventListener('click', prevPageButtonClick);
+
+    // constructing url of interest
+    let reqUrl = `/posts/${selection}?page=${currentPage}`;
+
+    fetch(reqUrl)
+        .then(response => response.json())
+        .then(data => {
+            let posts = data.posts;
+            let total_pages = data.total_pages;
+            document.querySelector('#posts').innerHTML = "";
+            // for each post received from server - creating post and rendering on webpage
+            posts.forEach(post => {
+                const id = post.id;
+                const poster = post.poster;
+                const content = post.content;
+                const timestamp = post.timestamp;
+                let likes = post.likes;
+                const owned = post.owned;
+                const liked = post.liked;
+                let posting = document.createElement('div');
+                posting.setAttribute('class', 'post');
+                posting.setAttribute('id', id);
+                posting.innerHTML = `<div class="user">${poster}</div> <div class="post-content">${content}</div> 
+                <div class="timestamp">${timestamp}</div> `;
+                const likesContent = document.createElement('div');
+                likesContent.setAttribute('class', 'likes');
+                likesContent.innerHTML = `<div class="like-count">${likes}</div>`;
+                const likeButton = document.createElement('div');
+                likeButton.setAttribute('class', 'like-btn');
+                // setting heart (like) state
+                if (liked) {
+                    likeButton.innerHTML = `<i class="fa-solid fa-heart" style="font-size:20px;color:#cc241d;"></i>`;
+                } else {
+                    likeButton.innerHTML = `<i class="fa-regular fa-heart" style="font-size:20px;"></i>`;
+                };
+                // if clicked handling like in like function
+                likeButton.addEventListener('click', () => {
+                    like_handling(id);
+                });
+                likesContent.appendChild(likeButton);
+                posting.appendChild(likesContent);
+                // if logged-in user is the post author - adding editing feature
+                if (owned) {
+                    let editButton = document.createElement('div');
+                    editButton.setAttribute('class', 'edit-btn');
+                    editButton.textContent = 'Edit';
+                    posting.appendChild(editButton);
+                }
+                // finally appending constructed post to webpage
+                document.querySelector('#posts').appendChild(posting);
+            })
+            // attaching listeners to user nicknames and edit button clicks
+            attachListeners();
+
+            // showing/hiding pagination links based on received data
+            const nextPage = document.querySelector('#next-page');
+            const prevPage = document.querySelector('#prev-page');
+
+            if (data.has_next) {
+                nextPage.style.display = 'block';
+            } else {
+                nextPage.style.display = 'none';
+            }
+            if (data.has_previous) {
+                prevPage.style.display = 'block';
+            } else {
+                prevPage.style.display = 'none';
+            }
+
+            // reassining global variables to relevant values and attaching listeners to pagination links
+            currPage = currentPage;
+            total = total_pages;
+            selected = selection;
+            nextPage.addEventListener('click', nextPageButtonClick);
+            prevPage.addEventListener('click', prevPageButtonClick);
+        });
 
 };
 
+// if next page is clicked checking current is not last and loading posts for next page
+const nextPageButtonClick = () => {
+    if (currPage == total) {
+        return;
+    } else {
+        currPage++;
+        load_posts(selected, currPage);
+    };
+};
+// if previous page is clicked checking it's not the first page and loading posts for previous page
+const prevPageButtonClick = () => {
+    if (currPage > 1) {
+        currPage--;
+        load_posts(selected, currPage);
+    };
+};
+
+// listeners for users and edit buttons
+function attachListeners() {
+    const userList = document.querySelectorAll(".user");
+    for (let i = 0; i < userList.length; i++) {
+        let nickname = userList[i].innerText;
+        userList[i].addEventListener('click', () => load_profile(nickname));
+        };
+
+    const editBtns = document.querySelectorAll('.edit-btn');
+    for (let i = 0; i < editBtns.length; i++) {
+        let id = editBtns[i].parentElement.id;
+        editBtns[i].addEventListener('click', () => edit_post(id));
+        };
+};
+
+// if loading profile
 function load_profile(username) {
-    document.querySelector('#new-post').style.display = 'none';
-    document.querySelector('#user-info').style.display = 'block';
-    let reqUrl = `${username}/`;
+    // receiving data from server and rendering on page
+    let reqUrl = `${username}`;
     fetch(reqUrl)
     .then(response => response.json())
     .then (data => {
-        console.log(data)
         const username = data.member;
         const following = data.following;
         const followers = data.followers;
         const connected = data.connected;
         document.querySelector('#user-info').innerHTML = `<div style="font-size: 24px;">${username}</div> <div> followers: ${followers.length} following: ${following.length} </div>`;
+        
+        // if profile loaded not of logged-in user attaching follow/unfollow button
         if (username !== document.querySelector('#user').textContent) {
             const followButton = document.createElement('button');
             followButton.setAttribute('id', 'follow-btn');
@@ -126,9 +208,12 @@ function load_profile(username) {
             document.querySelector('#user-info').appendChild(followButton);
         };
     });
+    // loading posts selection where author is user which profile is loaded
     load_posts(username);
 };
 
+
+// if followed/unfollowed letting server know and changing follow button state, then reloading profile
 function follow_handling(username) {
     if (document.querySelector('#follow-btn').textContent == 'Follow') {
         let reqUrl = `${username}/`;
@@ -140,7 +225,7 @@ function follow_handling(username) {
         });
         setTimeout(function() {
             load_profile(username);
-        }, 100);
+        }, 50);
         document.querySelector('#follow-btn').textContent = 'Unfollow';
     } else {
         let reqUrl = `${username}/`;
@@ -152,17 +237,20 @@ function follow_handling(username) {
         });
         setTimeout(function() {
             load_profile(username);
-        }, 100);
+        }, 50);
         document.querySelector('#follow-btn').textContent = 'Follow';
     };
 };
 
+// if like/unlike post button clicked
 function like_handling(id) {
+    // getting post id, selecting relevant components in that post
     const post = document.getElementById(`${id}`);
     let likes = post.querySelector('.likes');
     let count = likes.querySelector('.like-count');
     const likeButton = likes.querySelector('.like-btn');
     let liked = Boolean;
+    // change count and heart (like) button on page
     if (likeButton.querySelector('i').className == "fa-regular fa-heart") {
         liked = true
         likeButton.innerHTML = `<i class="fa-solid fa-heart" style="font-size:20px;color:#cc241d;"></i>`;
@@ -172,6 +260,7 @@ function like_handling(id) {
         likeButton.innerHTML = `<i class="fa-regular fa-heart" style="font-size:20px;"></i>`;
         count.textContent = parseInt(count.textContent) - 1;
     }
+    // let server know about change
     let reqUrl = `posts/edit/${id}`;
     fetch(reqUrl, {
         method: 'PUT',
@@ -182,16 +271,25 @@ function like_handling(id) {
     .then(response => console.log(response));
 };
 
-
+// if edit post was clicked
 function edit_post(id) {
+    // selecting relevant post and its components
     const post = document.getElementById(`${id}`);
     const content = post.querySelector('.post-content');
+
+    // creating textarea and save button, setting textarea value to current post content
     const textarea = document.createElement('textarea');
     const saveBtn = document.createElement('button');
     saveBtn.textContent = 'Save';
     textarea.value = content.innerHTML;
+
+    // replacing post content with textarea with content
     content.replaceWith(textarea);
+    //hiding edit button, appending save button
     post.querySelector('.edit-btn').style.display = 'none';
+    post.appendChild(saveBtn);
+
+    // when save is clicked letting server know of changes
     saveBtn.addEventListener('click', () => {
         updatedContent = textarea.value;
         let reqUrl = `posts/edit/${id}`;
@@ -202,10 +300,12 @@ function edit_post(id) {
             })
         })
         .then(response => console.log(response));
+
+        // setting post content to updated textarea value and replacing textarea with content back
         content.innerHTML = textarea.value;
         textarea.replaceWith(content);
+        // showing edit button again, removing save button
         post.querySelector('.edit-btn').style.display = 'block';
         saveBtn.remove();
     });
-    post.appendChild(saveBtn);
 };
--- network/views.py
+++ network/views.py
@@ -7,12 +7,16 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.contrib.auth.decorators import login_required
 from django.db.models import F
+from django.core.paginator import Paginator
+from datetime import timedelta
+from django.utils import timezone
 
 from .models import User, Post, Member, Like
 
 
 @csrf_exempt
 def index(request):
+    # handling new posts
     if request.method == "POST":
         if not request.user.is_authenticated:
             return JsonResponse({"error": "Authentication required."}, status=400)
@@ -33,17 +37,25 @@ def index(request):
 
 @csrf_exempt
 def profile(request, username):
+    # getting requested profile from db
     try:
         profile = Member.objects.get(user__username=username)
     except (Member.DoesNotExist):
        return JsonResponse({"error": "Member doesn't exist."}, status=400)
     
+    # getting logged in user member creds
     user_profile = Member.objects.get(user=request.user)
 
+    # if follow/unfollow data received from client
     if request.method == "POST":
+
+        # can't follow/unfollow yourself
         if profile == user_profile:
             return HttpResponse(status=400)
+        
         data = json.loads(request.body)
+
+        # handling following
         if data.get("followed") is True:
             if user_profile not in profile.followers.all():
                 profile.followers.add(user_profile)
@@ -54,9 +66,10 @@ def profile(request, username):
                 user_profile.following.remove(profile)
         profile.save()
         user_profile.save()
+
         return HttpResponse(status=204)
 
-    
+    # in GET, returns profile info and state of followed(connected) by logged user
     connected = bool
     if profile in user_profile.following.all():
         connected = True
@@ -68,24 +81,60 @@ def profile(request, username):
     return JsonResponse(serialized_profile, safe=False)
 
 
+# loading posts
 def posts(request, selection):
+
+    serialized_list = []
+    liked = bool
+    # if all posts selected getting all
     if selection == "all":
         post_objects = Post.objects.filter()
+
+    # followed users posts selection
     elif selection == "following":
         if not request.user.is_authenticated:
             return JsonResponse({"error": "Authentication required."}, status=400)
         authors = User.objects.filter(member__in=request.user.member.following.all())
         post_objects = Post.objects.filter(author__in=authors)
+
+    # popular posts selection (displayed by default in index.html)
+    elif selection == "popular":
+        # getting 5 posts in the last two weeks ordered by favs (likes) in descending order
+        one_month_ago = timezone.now() - timedelta(days=14)
+        recent_posts = Post.objects.filter(date__gte=one_month_ago)
+        top_recent_posts = recent_posts.order_by('-favs')[:5]
+
+        # preparing posts and returning them in json
+        for post in top_recent_posts:
+            serialized = post.serialize()
+            try:
+                Like.objects.get(to_post=post, by_user=request.user)
+                liked = True
+            except Like.DoesNotExist:
+                liked = False
+            serialized["liked"] = liked
+            if post.author == request.user:
+                serialized["owned"] = True
+            else:
+                serialized["owned"] = False
+            serialized_list += [serialized]
+        return JsonResponse({'posts': serialized_list,
+                             'total_pages': 1}, safe=False)
+    
+    # posts selection by specific user
     else:
         try:
             post_objects = Post.objects.filter(author__username=selection)
         except (KeyError, Post.DoesNotExist):
             return JsonResponse({"error": "Selection doesn't exist."}, status=400)
     
-    liked = bool
-    posts = post_objects.order_by("-date").all()
-    serialized_list = []
-    for post in posts:
+    # pagination
+    paginator = Paginator(post_objects.order_by("-date").all(), 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # preparing posts for json transfer (serializing, like by logged user state, author=logged user state)
+    for post in page_obj:
         serialized = post.serialize()
         try:
             Like.objects.get(to_post=post, by_user=request.user)
@@ -99,13 +148,21 @@ def posts(request, selection):
             serialized["owned"] = False
         serialized_list += [serialized]
 
-    return JsonResponse(serialized_list, safe=False)
+    # responding with list of posts, pagination states
+    return JsonResponse({
+        'posts': serialized_list,
+        'has_next': page_obj.has_next(),
+        'has_previous': page_obj.has_previous(),
+        'total_pages': paginator.num_pages
+    }, safe=False)
 
 
 @csrf_exempt
 @login_required
+# handling posts editing and likes
 def edit_post(request, id):
     if request.method == 'PUT':
+        # getting post from db
         try:
             post = Post.objects.get(pk=id)
         except (Post.DoesNotExist):
@@ -113,20 +170,23 @@ def edit_post(request, id):
     
         data = json.loads(request.body)
 
+        # if editing attempt checking for ownership and setting edited content as post content
         if data.get("content") is not None:
             if post.author == request.user:
                 post.content = data["content"]
             else:
                 return JsonResponse({"error": "Invalid credentials to edit this post."}, status=400)
-            
+        
+        # if post is liked/unliked
         if data.get("like") is not None:
+            # if liked creating Like instance and updating post's like count
             if data.get("like") is True:
                 try:
                     Like.objects.create(to_post=post, by_user=request.user)
                     post.favs = F('favs') + 1
                 except IntegrityError:
                     return JsonResponse({"error": "Post already liked."}, status=400)
-
+            # if unliked deleting Like instance updating post's like count
             else:
                 try:
                     Like.objects.get(to_post=post, by_user=request.user).delete()
@@ -134,10 +194,12 @@ def edit_post(request, id):
                 except (Like.DoesNotExist):
                     return JsonResponse({"error": "Post already unliked."}, status=400)
         
+        # saving post changes
         post.save()
 
         return HttpResponse(status=204)
-        
+    
+    # only PUT method allowed for editing and likes
     else:
         return JsonResponse({"error": "PUT request required."}, status=400)
 
